11d7c8986e3d4e8ddb8b7eddf1fceec55c2bf579
leap <Bunleab.sok@student.cadt.edu.kh>
2025-03-26 11:15:05 +0700

    update

--- scripts/count_files.sh
+++ scripts/count_files.sh
@@ -2,3 +2,13 @@
 
 # TODO: Implement file counting in a directory.
 # - Log the action in `script.log`.
+echo "Enter directory path:"
+read dir
+
+if [ -d "$dir" ]; then
+    count=$(ls -1 "$dir" | wc -l)
+    echo "Number of files: $count"
+else
+    echo "Directory not found!"
+fi
+
